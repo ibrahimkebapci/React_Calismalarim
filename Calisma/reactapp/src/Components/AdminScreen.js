1d9119f947221a6860832dd0eac91dcf96939ca4
ibrahimkebapci <ibrahim_kebapci@hotmail.com>
2024-08-08 02:13:39 +0300

    Update

--- Calisma/reactapp/src/Components/AdminScreen.js
+++ Calisma/reactapp/src/Components/AdminScreen.js
@@ -1,74 +1,124 @@
-// src/Components/AdminScreen.js
-import React from 'react';
+import React, { useState } from 'react';
 import './AdminScreen.css';
 
 const AdminScreen = () => {
+  const users = [
+    { id: 1, name: 'İbrahim', password: 'Kebapci', status: 'Aktif', role: 'Broşür', group: 'Mehmet Yıldız' },
+    { id: 2, name: 'Murat', password: 'Taşçı', status: 'Aktif', role: 'Kutu', group: 'Halis Abi' },
+    { id: 3, name: 'Ahmet', password: 'Yavuz', status: 'Pasif', role: 'Medrese İçi', group: 'Onur Abi' },
+    { id: 4, name: 'Kenan', password: 'Şanlı', status: 'Pasif', role: 'Asosyal', group: 'Sinan Abi' },
+    { id: 5, name: 'Mesut', password: 'Petek', status: 'Aktif', role: 'Sosyal', group: 'Geylani Abi' },
+    { id: 6, name: 'Yeni Kullanıcı', password: 'Yeni Şifre', status: 'Durum', role: 'Yeni Vazife', group: 'Yeni Ders Grubu' },
+    { id: 7, name: 'Kullanıcı 7', password: 'Şifre 7', status: 'Aktif', role: 'Rol 7', group: 'Grup 7' },
+    { id: 8, name: 'Kullanıcı 8', password: 'Şifre 8', status: 'Aktif', role: 'Rol 8', group: 'Grup 8' },
+    { id: 9, name: 'Kullanıcı 9', password: 'Şifre 9', status: 'Aktif', role: 'Rol 9', group: 'Grup 9' },
+    { id: 10, name: 'Kullanıcı 10', password: 'Şifre 10', status: 'Aktif', role: 'Rol 10', group: 'Grup 10' },
+    { id: 11, name: 'Kullanıcı 11', password: 'Şifre 11', status: 'Aktif', role: 'Rol 11', group: 'Grup 11' },
+  ];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const usersPerPage = 10;
+
+  const filteredUsers = users.filter(user =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(filteredUsers.length / usersPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const handleSelectUser = (id) => {
+    if (selectedUsers.includes(id)) {
+      setSelectedUsers(selectedUsers.filter(userId => userId !== id));
+    } else {
+      setSelectedUsers([...selectedUsers, id]);
+    }
+  };
+
+  const handleSelectAllUsers = () => {
+    if (selectedUsers.length === currentUsers.length) {
+      setSelectedUsers([]);
+    } else {
+      setSelectedUsers(currentUsers.map(user => user.id));
+    }
+  };
+
   return (
     <div className="admin-screen">
       <div className="admin-screen-header">
         <h1>Admin Ekranı</h1>
         <div className="button-group">
-          <button className="add-customer-btn">Kullanıcı Ekle</button>
-          <button className="delete-customer-btn">Kullanıcı Sil</button>
-          <button className="update-customer-btn">Kullanıcı Güncelle</button>
+          <button className="add-customer-btn">Müşteri Ekle</button>
+          <button className="delete-customer-btn">Müşteri Sil</button>
+          <button className="update-customer-btn">Güncelle</button>
         </div>
       </div>
-      <table className="admin-table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Kullanıcı Adı</th>
-            <th>Şifre</th>
-            <th>Hesap Durumu</th>
-            <th>Vazife</th>
-            <th>Ders Grubu</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Verilerinizi buraya ekleyin */}
-          <tr>
-            <td>1</td>
-            <td>İbrahim</td>
-            <td>Kebapci</td>
-            <td>Aktif</td>
-            <td>Broşür</td>
-            <td>Mehmet Yıldız</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Murat</td>
-            <td>Taşçı</td>
-            <td>Aktif</td>
-            <td>Kutu</td>
-            <td>Halis Abi</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Ahmet</td>
-            <td>Yavuz</td>
-            <td>Pasif</td>
-            <td>Medrese İçi</td>
-            <td>Onur Abi</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Kenan</td>
-            <td>Şanlı</td>
-            <td>Pasif</td>
-            <td>Asosyal</td>
-            <td>Sinan Abi</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Mesut</td>
-            <td>Petek</td>
-            <td>Açık</td>
-            <td>Aktif</td>
-            <td>Geylani Abi</td>
-          </tr>
-          {/* Daha fazla satır ekleyebilirsiniz */}
-        </tbody>
-      </table>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Ara..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="table-container">
+        <table className="admin-table">
+          <thead>
+            <tr>
+              <th>
+                <input
+                  type="checkbox"
+                  onChange={handleSelectAllUsers}
+                  checked={selectedUsers.length === currentUsers.length}
+                  style={{ transform: 'scale(0.8)' }}
+                />
+              </th>
+              <th>Kullanıcı ID</th>
+              <th>Kullanıcı Adı</th>
+              <th>Şifre</th>
+              <th>Hesap Durumu</th>
+              <th>Vazife</th>
+              <th>Ders Grubu</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentUsers.map(user => (
+              <tr key={user.id}>
+                <td>
+                  <input
+                    type="checkbox"
+                    onChange={() => handleSelectUser(user.id)}
+                    checked={selectedUsers.includes(user.id)}
+                    style={{ transform: 'scale(0.8)' }}
+                  />
+                </td>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.password}</td>
+                <td>{user.status}</td>
+                <td>{user.role}</td>
+                <td>{user.group}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="pagination">
+        {pageNumbers.map(number => (
+          <button key={number} onClick={() => paginate(number)} className={currentPage === number ? 'active' : ''}>
+            {number}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
--- Calisma/reactapp/src/Components/Dashboard.js
+++ Calisma/reactapp/src/Components/Dashboard.js
@@ -13,12 +13,12 @@ class Dashboard extends React.Component {
       <div>
         <h2>Hizmetlerin Durumu</h2>
         <div className="cards-container">
-          <Card title="Bekleyenler" count="1345" color="#28a745" />
-          <Card title="Şu Anda Yapılanlar" count="12456" color="#6f42c1" />
-          <Card title="Tamamlananlar" count="21" color="#fd7e14" />
-          <Card title="Yedek_1" count="1220" color="#17a2b8" />
+          <Card title="Bekleyenler" count="35" color="#28a745" />
+          <Card title="Şu Anda Yapılanlar" count="50" color="#6f42c1" />
+          <Card title="Tamamlananlar" count="120" color="#fd7e14" />
+          <Card title="Yedek_1" count="45" color="#17a2b8" />
           <Card title="Yedek_2" count="65" color="#343a40" />
-          <Card title="Yedek_3" count="9876" color="#6c757d" />
+          <Card title="Yedek_3" count="110" color="#6c757d" />
         </div>
         <LatestEvents /> {/* LatestEvents bileşenini ekliyoruz */}
         <UserStats /> {/* UserStats bileşenini ekliyoruz */}
